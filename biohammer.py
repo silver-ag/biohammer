7c24c3dc86693f47f3ce4069fd3ab07a78cfe589
Asa <asaglenister@protonmail.com>
2024-05-10 12:24:25 +0100

    move to sched-based system, much more precise timings

--- biohammer.py
+++ biohammer.py
@@ -1,10 +1,14 @@
 import pygame as pg
-import mido
+
+import rtmidi
+from rtmidi.midiconstants import NOTE_ON, NOTE_OFF
+import sched
+import time
+
 from threading import Thread
 from queue import Queue
 from time import perf_counter_ns
 from meatflower import MeatflowerGui, Cell, Text, EditableText, Row, Column, Table, Dropdown, Menu
-
 
 class Editor:
     def __init__(self):
@@ -12,9 +16,29 @@ class Editor:
         self.screen = pg.display.set_mode((1280, 720))
         self.gui = MeatflowerGui((1280,720))
         self.clock = pg.time.Clock()
-        self.midi_out = mido.open_output()
-        self.player_control = Queue()
-        self.player_thread = Thread(target=self.player, args = [self.player_control], daemon = True).start()
+        self.midi_out = rtmidi.MidiOut()
+        self.set_midi_port(0)
+        self.midi_out.set_port_name("biohammer")
+        self.scheduler = sched.scheduler(time.time, time.sleep)
+        self.scheduler_thread = Thread(target=self.run_scheduler,daemon=True).start()
+        # annotate the scheduler with some extra data we'll use to account for the loop times being in logical format unaware of their bpm
+        self.scheduler.start_t = 0
+        self.scheduler.scheduled_up_to = 0
+        self.scheduler.scheduler_cursor = 1
+    def run_scheduler(self):
+        while True:
+            self.scheduler.run()
+    def clear_schedule(self):
+        for event in self.scheduler.queue:
+            self.scheduler.cancel(event)
+        self.scheduler = sched.scheduler(time.time, time.sleep)
+        self.scheduler_thread = Thread(target=self.run_scheduler,daemon=True).start()
+        self.scheduler.start_t = time.time()
+        self.scheduler.scheduled_up_to = self.scheduler.start_t
+        self.scheduler.scheduler_cursor = 0
+    def set_midi_port(self, index):
+        self.midi_out_port = self.midi_out.open_port(index)
+        self.midi_out_name = self.midi_out.get_ports()[index]
     def edit(self, loop):
         title = self.gui.add_element(EditableText, (0,0), loop.title, colour = (0,0,0))
         length_value = self.gui.add_element(EditableText, (0,0), str(loop.length))
@@ -30,17 +54,18 @@ class Editor:
         controls = self.gui.add_element(Row, (0,0), [play_button, instrument_menu, add_track_button, bpm_control])
         layout = self.gui.add_element(Column, (0,0), [topbar, edit_table, controls])
 
-        self.player_control.put(['loop',loop])
         bpm = 0
         playing = False
-        t = 0
+        self.scheduler.start_t = 0
+        self.scheduler.scheduled_up_to = 0
+        self.scheduler.scheduler_cursor = 1
         
         while True:
             try:
                 new_bpm = int(bpm_value.text)
                 if new_bpm != bpm:
                     bpm = new_bpm
-                    self.player_control.put(['bpm', bpm])
+                    self.clear_schedule()
                 bpm_value.colour = (128,128,128)
             except:
                 bpm_value.colour = (200,0,0)
@@ -57,6 +82,18 @@ class Editor:
             except Exception as e:
                 print(e)
                 length_value.colour = (200,0,0)
+
+            t = time.time()
+            if playing and self.scheduler.scheduled_up_to < t + 1:
+                for i in range(10):
+                    notes = loop.events_at_time(self.scheduler.scheduler_cursor)
+                    s_t = 0
+                    for note in notes:
+                        s_t = self.scheduler.start_t + (self.scheduler.scheduler_cursor * (60/bpm if bpm > 0 else 1))
+                        self.scheduler.enterabs(s_t, 0, self.midi_out.send_message, argument=([NOTE_ON,note,127],))
+                    self.scheduler.scheduled_up_to = s_t
+                    self.scheduler.scheduler_cursor += 1
+                
 
             for event in pg.event.get():
                 if event.type == pg.QUIT:
@@ -81,7 +118,7 @@ class Editor:
                         elif elem == play_button:
                             self.gui.select_element(None)
                             playing = not playing
-                            self.player_control.put(['play' if playing else 'stop'])
+                            self.clear_schedule()
                             play_button.set_text('pause ||' if playing else 'play >')
                         elif elem == add_track_button:
                             self.gui.select_element(None)
@@ -101,7 +138,7 @@ class Editor:
                 bottomcell = edit_table.children[(loop.player_head + 1, len(loop.events)-1)].rect
                 pg.draw.line(self.screen, (250,250,250), topcell.midtop, bottomcell.midbottom, width=2)
             pg.display.flip()
-            self.clock.tick()
+            self.clock.tick(60)
     def recalculate_edit_table(self, loop):
         edit_table = self.gui.add_element(Table, (0,0), (loop.length + 2, len(loop.events)), padding=0.5)
         delete_track_buttons = {}
@@ -109,43 +146,12 @@ class Editor:
         for track in loop.events:
             edit_table.children[(0, y)] = self.gui.add_element(EditableText, (0,0), track)
             for x in range(loop.length):
-                edit_table.children[(x+1, y)] = self.gui.add_element(Cell, (0,0), (30,30), str(loop.events[track][x].note if x in loop.events[track] else ''))
+                edit_table.children[(x+1, y)] = self.gui.add_element(Cell, (0,0), (30,30), str(loop.events[track][x] if x in loop.events[track] else ''))
             btn = self.gui.add_element(Cell, (0,0), (30,30), 'X', colour = (200,200,200))
             edit_table.children[(loop.length+1, y)] = btn
             delete_track_buttons[btn] = track
             y += 1
         return edit_table, delete_track_buttons
-    def player(self, q):
-        playing = False
-        loop = None
-        ns_per_beat = 1000000000 # 1 second
-        start_t = 0
-        while True:
-            if playing and (loop is not None):
-                ns = perf_counter_ns()
-                t = ns - start_t
-                if t > ns_per_beat:
-                    start_t = perf_counter_ns() - (t - ns_per_beat)
-                    notes = loop.step()
-                    for note in notes:
-                        #msg = mido.Message('note_on', note = note)
-                        self.midi_out.send(note)
-                        #print(msg)
-            if not q.empty():
-                instruction = q.get()
-                print(instruction)
-                if instruction[0] == 'play':
-                    playing = True
-                    start_t = perf_counter_ns()
-                elif instruction[0] == 'stop':
-                    playing = False
-                    if loop is not None:
-                        loop.reset()
-                elif instruction[0] == 'bpm':
-                    ns_per_beat = (1/instruction[1])*60*1000000000
-                    print(ns_per_beat)
-                elif instruction[0] == 'loop':
-                    loop = instruction[1]
 
 class Loop:
     def __init__(self, length, tracks, title = None):
@@ -173,7 +179,7 @@ class Loop:
         if value is None:
             self.events[track].pop(t, None)
         else:
-            self.events[track][t] = mido.Message('note_on', note=int(value))
+            self.events[track][t] = int(value)
     def add_track(self, name):
         if name in self.events:
             self.add_track(name + '+')
